5b406f5ed7eaef64d5f4bd81d86f7d9f7edaf64f
NIDALMANSOURI <nidalmansouri95@gmail.com>
2023-05-27 10:59:00 -0700

    put

---
+++ _put.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * _puts - print a string with a new line
+ * @str: string to print
+ * Return: string
+ */
+int _puts(char *str)
+{
+	char *a = str;
+
+	while (*str)
+		_putchar(*str++);
+	return (str - a);
+}
+
+/**
+ * _putchar - writes the character c to sdout
+ * @c: the character to write
+ * Return: 1 for success, -1 for failure
+ */
+int _putchar(int c)
+{
+	static int i;
+	static char buf[OUTPUT_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= OUTPUT_BUF_SIZE)
+	{
+		write(1, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
