4d6df19afe375e5d4085b39a3ec709f6d08481ba
wafaetaouil <wafaetaouil@gmail.com>
2023-05-28 11:57:44 -0700

    task0

--- simple_printers.c
+++ simple_printers.c
@@ -53,7 +53,7 @@ int print_rot13(va_list ap, params_t *params)
 	int i, index;
 	int count = 0;
 	char arr[] =
-		"NOPQRSTUVWXYZABCDEFGHIJKLM	nopqrstuvwxyzabcdefghijklm";
+		"NOPQRSTUVWXYZABCDEFGHIJKLM             nopqrstuvwxyzabcdefghijklm";
 	char *a = va_arg(ap, char *);
 	(void)params;
 
