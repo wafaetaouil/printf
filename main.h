68b837bd1fd8d49e0ad1339ff018342d2f62aff4
NIDALMANSOURI <nidalmansouri95@gmail.com>
2023-05-28 11:18:52 -0700

    main

--- main.h
+++ main.h
@@ -33,23 +33,26 @@
 typedef struct parameters
 {
 	unsigned int unsign : 1;
+
 	unsigned int plus_flag : 1;
 	unsigned int space_flag : 1;
 	unsigned int hashtag_flag : 1;
 	unsigned int zero_flag : 1;
 	unsigned int minus_flag : 1;
-	unsigned int width : 1;
+
+	unsigned int width;
 	unsigned int precision;
+
 	unsigned int h_modifier : 1;
 	unsigned int l_modifier : 1;
 } params_t;
 
 /**
- * struct token - struct token
+ * struct specifier - struct token
  * @specifier: format token
  * @f: function associated
  */
-typedef struct token
+typedef struct specifier
 {
 	char *specifier;
 	int (*f)(va_list, params_t *);
