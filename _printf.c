2b6a123ac91522a33330b59119c465897bcc1730
NIDALMANSOURI <nidalmansouri95@gmail.com>
2023-05-28 10:24:55 -0700

    printf

--- _printf.c
+++ _printf.c
@@ -9,11 +9,11 @@
 int _printf(const char *format, ...)
 {
 	int s = 0;
-	va_list a;
+	va_list ap;
 	params_t params = PARAMS_INIT;
 	char *p, *st;
 
-	va_start(a, format);
+	va_start(ap, format);
 
 	if (!format || (format[0] == '%' && !format[1]))
 		return (-1);
@@ -21,7 +21,7 @@ int _printf(const char *format, ...)
 		return (-1);
 	for (p = (char *)format; *p; p++)
 	{
-		init_params(&params, a);
+		init_params(&params, ap);
 		if (*p != '%')
 		{
 			s += _putchar(*p);
@@ -33,18 +33,18 @@ int _printf(const char *format, ...)
 		{
 			p++;
 		}
-		p = get_width(p, &params, a);
-		p = get_precision(p, &params, a);
+		p = get_width(p, &params, ap);
+		p = get_precision(p, &params, ap);
 		if (get_modifier(p, &params))
 			p++;
 		if (!get_specifier(p))
 			s += print_from_to(st, p,
 					params.l_modifier || params.h_modifier ? p - 1 : 0);
 		else
-			s += get_print_func(p, a, &params);
+			s += get_print_func(p, ap, &params);
 	}
 	_putchar(BUF_FLUSH);
-	va_end(a);
+	va_end(ap);
 	return (s);
 }
 
